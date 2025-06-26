ad0e60e28cc32f6711b80a7d0d278adee5614e09
raghavpathak <pathak.ins@gmail.com>
2025-06-26 15:41:28 +0530

    spring manage custom class objects

--- src/main/java/com/in28minute/App02HelloWorld.java
+++ src/main/java/com/in28minute/App02HelloWorld.java
@@ -6,12 +6,16 @@ public class App02HelloWorld {
     public static void main(String[] args) {
     //1. launching spring context 
     var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
-    
+
     //2. configure the things that are managed by spring - @configuration 
     // create configuration class using - @configuration
     // create bean with @bean 
 
     // retriveing spring bean 
     System.out.println(context.getBean("name"));
+    System.out.println(context.getBean("age"));
+    System.out.println(context.getBean("person"));
+    System.out.println(context.getBean("address"));
+
     }
 }
--- src/main/java/com/in28minute/HelloWorldConfiguration.java
+++ src/main/java/com/in28minute/HelloWorldConfiguration.java
@@ -3,10 +3,26 @@ package com.in28minute;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+// record class(data carried final classes) 
+// auto generate constructor/ hashcode()/ tostring metod/and getter methods
+record Person(String name , int age){};
+record Address(String firstline, String city){};
 @Configuration         // all the spring beans are defined and maganed here 
 public class HelloWorldConfiguration {
     @Bean
     public String name(){
         return "raghav";
+    }
+    @Bean
+    public int age(){
+        return 25;
+    }
+    @Bean
+    public Person person(){
+        return new Person("Rishabh pathak", 23);
+    }
+    @Bean
+    public Address address(){
+        return new Address("avas vikas", "chandausi");
     }
 }
