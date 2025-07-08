f851d9667df60129ed3ade2c6e1f8465757b6af7
raghavpathak <pathak.ins@gmail.com>
2025-07-08 16:21:43 +0530

    Dependency Injection using Field Injection

--- src/main/java/com/in28minute/learningspringframework/examples/a1/DepInjectionLauncherApplication.java
+++ src/main/java/com/in28minute/learningspringframework/examples/a1/DepInjectionLauncherApplication.java
@@ -3,14 +3,36 @@ package com.in28minute.learningspringframework.examples.a1;
 
 import java.util.Arrays;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Component
+class YoureBusinessClass{
+
+    @Autowired                 // @autowired on any field  -> flied injection .
+    Dependency1 dependency1;
+
+    @Autowired                 
+    Dependency2 dependency2;
+
+    public String toString(){
+        return "using " + dependency1 + " and "+ dependency2;
+    }
+}
+@Component
+class Dependency1{
+
+}
+@Component
+class Dependency2{
+}
 
 @Configuration      // since it @confifuration , this class is a bean also .
 @ComponentScan      // no need to mention the package if @componentScan in same package  .
-// this Component in pachman and  ComponentScan here will auto create and scan spring bean for us
 public class DepInjectionLauncherApplication{
     
     public static void main(String[] args) {
@@ -18,10 +40,11 @@ public class DepInjectionLauncherApplication{
 
         try(var context = new AnnotationConfigApplicationContext
         (DepInjectionLauncherApplication.class);){
-            
+
             Arrays.stream(context.getBeanDefinitionNames())
                     .forEach(System.out::println);
+            
+            System.out.println(context.getBean(YoureBusinessClass.class));
         }
-        
     }
 }
